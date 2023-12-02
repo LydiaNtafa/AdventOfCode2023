2ff2d551172970dbf1030902a578e72f747353d0
LydiaNtafa <lydiadafa@gmail.com>
2023-12-02 17:01:38 +0000

    text is being converted to array of arrays with every game presented as [ ID int, subsetOfCubes1 str, subsetOfCubes2 str, etc]

--- day2/CubeGame.js
+++ day2/CubeGame.js
@@ -11,7 +11,30 @@ class CubeGame {
         return input;
     };
 
+    calculateGames= () => {
+        sum = 0;
+        const splitText = this.doc.split("\n");
+        const arrayOfGameArrays = splitText.map(this.createGameArray);
+        
+    }
+
+    createGameArray = (str) => {
+        var nonNumberRegex = /[^0-9]/g;
+        var gameArray = [];
+        str = str.split(":");
+        gameArray[0]= parseInt(str[0].replace(nonNumberRegex, ''));
+        str = str[1].split(";")
+        console.log(str);
+        for (let i=0; i<str.length; i++) {
+            gameArray[i+1]=str[i];
+        }
+        return gameArray;
+    }
+
 
 }
+
+const path = "./mockInput.txt";
+const Game = new CubeGame(path);
 
 module.exports = CubeGame;
