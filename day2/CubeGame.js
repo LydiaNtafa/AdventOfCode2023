6e5f97b49a86ab7164dbe32f436abd4811913fce
LydiaNtafa <lydiadafa@gmail.com>
2023-12-02 17:24:36 +0000

    findCubes() find the correct number of cubes when the coliur and the subse tis given

--- day2/CubeGame.js
+++ day2/CubeGame.js
@@ -15,7 +15,12 @@ class CubeGame {
         sum = 0;
         const splitText = this.doc.split("\n");
         const arrayOfGameArrays = splitText.map(this.createGameArray);
-        
+        for (let i=0; i<arrayOfGameArrays.length; i++){
+            for (let j=1; j<arrayOfGameArrays[i].length; i++){
+                let redCubes = this.findCubes("red", arrayOfGameArrays[i][j]);
+                
+        }
+        return sum;
     }
 
     createGameArray = (str) => {
@@ -24,13 +29,19 @@ class CubeGame {
         str = str.split(":");
         gameArray[0]= parseInt(str[0].replace(nonNumberRegex, ''));
         str = str[1].split(";")
-        console.log(str);
         for (let i=0; i<str.length; i++) {
             gameArray[i+1]=str[i];
         }
         return gameArray;
     }
 
+    findCubes = (colour,subsetOfCubes) => {
+        let position = subsetOfCubes.search(colour);
+        if (position === -1) {
+            return 0;
+        }
+        return parseInt(subsetOfCubes.substring(position-3, position-1))
+    }
 
 }
 
