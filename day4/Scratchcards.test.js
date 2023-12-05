dae241953f820db3289131e07bf28174afe21d91
LydiaNtafa <lydiadafa@gmail.com>
2023-12-05 14:03:04 +0000

    first test to check each line generates an array of 2 arrays, 1 for winning numbers and one for numbers at hand

--- day4/Scratchcards.test.js
+++ day4/Scratchcards.test.js
@@ -6,4 +6,11 @@ describe("Scratchcards Game", () => {
         const Game = new Scratchcards(path);
         expect(Game.doc).toBeTruthy();
     });
+
+    it("converts each line into 2 arryays of numbers", () => {
+        const path = "./mockInput.txt";
+        const Game = new Scratchcards(path);
+        expect(Game.createGameArray(`Card   1: 41 48 83 86 17 |`+
+        ` 83 86  6 31 17  9 48 53\r`,)).toEqual([[41,48,83,86,17],[83,86,6,31,17,9,48,53]])
+    })
 })
